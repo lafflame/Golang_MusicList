0bce2e27c89708933144e398baafbc326163476f
Alexey Boldyrev <cooldock4play@gmail.com>
2025-01-30 22:41:43 +0300

    Лёгкие доработки кода

--- main.go
+++ main.go
@@ -10,12 +10,13 @@ import (
 	"time"
 )
 
+// Выводим все треки в начале программы и потом предлагаем выбор
 func main() {
-	//Выводим все треки в начале программы и потом предлагаем выбор
 	allTracks()
 	vibor()
 }
 
+// Выбор пользователем действий
 func vibor() {
 	//Главное меню
 	fmt.Println("\nВыберите действие:\n1.Добавить трек\n2.Выдать случайный трек\n3.Удалить трек\n4.Вывести все треки\n5.Выход\n")
@@ -26,31 +27,43 @@ func vibor() {
 	reader := bufio.NewReader(os.Stdin)
 	reader.ReadString('\n')
 
-	//Пользователь выбирает, что ему интересно
+	//Пользователь выбирает действие
 	switch choice {
 	case 1:
-		zapis()
-		prodolzhenie()
+		zapis() //Добавить трек
 	case 2:
-		random()
-		prodolzhenie()
+		random() //Выдать случайный трек
 	case 3:
-		fmt.Println("В разработке...")
+		fmt.Println("В разработке...") //Удалить трек
 		time.Sleep(2 * time.Second)
 		vibor()
 	case 4:
-		allTracks()
-		prodolzhenie()
+		allTracks() //Вывести все треки
 	case 5:
-		fmt.Println("Хорошего дня!")
-		time.Sleep(2 * time.Second)
+		fmt.Println("Хорошего дня!") //Выход
 		return
 	default:
 		fmt.Println("Некорректный выбор")
 		vibor()
 	}
+	prodolzhenie() //Если пользователь хочет выбрать ещё что-то
 }
 
+// Вопрос пользователю, чтобы не запускать программу заново после каждого действия
+func prodolzhenie() {
+	fmt.Println("Вы хотите продолжить? (y/n)")
+	var choice string
+	fmt.Scan(&choice)
+	if choice == "y" {
+		vibor()
+	} else {
+		fmt.Println("Хорошего дня!")
+		time.Sleep(2 * time.Second)
+		return
+	}
+}
+
+// Вывод всех треков на экран
 func allTracks() {
 	//Стандартная работа с файлом и отложенное закрытие
 	f, err := os.Open("Tracks.txt")
@@ -71,39 +84,36 @@ func allTracks() {
 	}
 }
 
+// Запись пользовательского трека в файл по стандарту "№ : "NameOfArtist - NameOfSong"
 func zapis() {
 	//Стандартная работа с файлом и отложенное закрытие
 	filePath := "Tracks.txt"
-	f, err := os.Open(filePath)
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
-		panic(err)
+		fmt.Println("Error:", err)
+		return
 	}
 	defer f.Close()
 
-	//Читаем пользовательский ввод - название группы
+	//Читаем имя исполнителя
 	fmt.Println("Введите имя исполнителя или название группы: ")
 	nameOfArtist, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 	nameOfArtist = strings.TrimSpace(nameOfArtist)
 
-	//Читаем пользовательский ввод - трек
+	//Читаем название трек
 	fmt.Println("Введите название трека: ")
 	trackName, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 	trackName = strings.TrimSpace(trackName)
 
 	//Добавляем номер трека в начало, тире между треком и названием группы
-	lastTrackNumber, err := lastTrackNumber(filePath)
+	lastTrackNumber, err := lastTrackNumber()
 	data := strconv.Itoa(lastTrackNumber+1) + ": " + nameOfArtist + " - " + trackName
-	f.WriteString(data)
-
-	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Println("Error:", err)
-		return
 	}
-	defer file.Close()
 
 	// Добавляем текст в конец файла
-	_, err = file.WriteString(data + "\n")
+	_, err = f.WriteString(data + "\n")
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
@@ -112,36 +122,7 @@ func zapis() {
 	}
 }
 
-func lastTrackNumber(file string) (int, error) {
-	//Стандартная работа с файлом и отложенное закрытие
-	f, err := os.Open("Tracks.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-	maxNumber := 0
-	for scanner.Scan() {
-		line := scanner.Text()
-		parts := strings.SplitN(line, ":", 2) // Разделяем строку на номер и название
-		if len(parts) < 2 {
-			continue // Если строка не содержит разделителя ":", пропускаем
-		}
-
-		// Пробуем преобразовать номер в число
-		number, err := strconv.Atoi(strings.TrimSpace(parts[0]))
-		if err == nil && number > maxNumber {
-			maxNumber = number
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		return 0, err // Возвращаем ошибку, если она произошла при чтении
-	}
-
-	return maxNumber, nil
-}
-
+// Вывод случайного трека
 func random() {
 	//Стандартная работа с файлом и отложенное закрытие
 	filePath := "Tracks.txt"
@@ -170,16 +151,33 @@ func random() {
 	fmt.Println(tracks[randomIndex])
 }
 
-//Функция вопросник нужна, чтобы не запускать программу заново после каждого действия
-func prodolzhenie() {
-	fmt.Println("Вы хотите продолжить? (y/n)")
-	var choice string
-	fmt.Scan(&choice)
-	if choice == "y" {
-		vibor()
-	} else {
-		fmt.Println("Хорошего дня!")
-		time.Sleep(2 * time.Second)
-		return
+// Определение последнего числа (кол-во строк) для работы с добавлением треков и определения кол-ва треков
+func lastTrackNumber() (int, error) {
+	//Стандартная работа с файлом и отложенное закрытие
+	f, err := os.Open("Tracks.txt")
+	if err != nil {
+		panic(err)
 	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	maxNumber := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, ":", 2) // Разделяем строку на номер и название
+		if len(parts) < 2 {
+			continue // Если длина строки меньше 2 - пропускаем
+		}
+
+		// Пробуем преобразовать номер в число
+		number, err := strconv.Atoi(strings.TrimSpace(parts[0]))
+		if err == nil && number > maxNumber {
+			maxNumber = number
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return 0, err // Возвращаем ошибку, если она произошла при чтении
+	}
+
+	return maxNumber, nil
 }
